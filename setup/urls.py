f03a90ec5d6bc2df87d16dd8f06783bd7aaf0f42
celioalves <craftsman7@gmail.com>
2023-04-10 14:25:58 -0300

    Adicionado caminho específico para o honeypot

--- setup/urls.py
+++ setup/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from escola.views import AlunosViewSet, CursosViewSet, MatriculaViewSet, ListaMatriculasAluno, ListaAlunosMatriculados
 from rest_framework import routers
 from django.conf import settings
@@ -11,8 +11,9 @@ router.register('cursos', CursosViewSet, basename='Cursos')
 router.register('matriculas', MatriculaViewSet, basename='Matriculas')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(router.urls) ),
+    path('controle-geral', admin.site.urls),
+    path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
+    path('', include(router.urls)),
     path('alunos/<int:pk>/matriculas/', ListaMatriculasAluno.as_view()),
     path('cursos/<int:pk>/matriculas/', ListaAlunosMatriculados.as_view())
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
